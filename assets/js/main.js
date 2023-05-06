56fadf1c580407d914d53bf4abde0762f5a313c9
pph <pph>
2023-05-06 17:12:29 +0630

    Added clients logos

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,7 +19,7 @@
     // Preloader
     $(window).on('load', function() {
         if ($('#preloader').length) {
-            $('#preloader').delay(30).fadeOut('slow', function() {
+            $('#preloader').delay(10).fadeOut('slow', function() {
                 $(this).remove();
             });
         }
